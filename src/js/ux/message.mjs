406475bb3e4bba638d1ba34452a9cee1142a2c41
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-10-15 13:24:54 +0200

    render toast function

--- src/js/ux/message.mjs
+++ src/js/ux/message.mjs
@@ -1,15 +1,38 @@
-export function createMessage(message) {
-  const modal = document.createElement('dialog');
-  const messageContainer = document.createElement('div');
-  const content = document.createElement('p');
-  const button = document.createElement('button');
+const container = document.querySelector('body');
 
-  modal.classList.add('position-absolute', 'top-50', 'left-50', 'translate-middle');
+export function createToast(message) {
+  const toastContainer = document.createElement('div');
+  const toast = document.createElement('div');
+  const toastHeader = document.createElement('div');
+  const headerTitle = document.createElement('strong');
+  const closeButton = document.createElement('button');
+  const toastBody = document.createElement('div');
 
-  messageContainer.append(content, button);
-  modal.append(messageContainer);
+  toastContainer.classList.add('toast-container', 'position-fixed', 'top-0', 'end-0', 'p-3');
+  headerTitle.classList.add('me-auto');
+  closeButton.classList.add('btn-close');
+  toast.classList.add('toast');
+  toastHeader.classList.add('toast-header');
+  toastBody.classList.add('toast-body');
+  closeButton.setAttribute('data-bs-dismiss', 'toast');
+  closeButton.setAttribute('type', 'button');
+  closeButton.setAttribute('aria-label', 'Close');
+  toast.setAttribute('id', 'liveToast');
+  toast.setAttribute('role', 'alert');
+  toast.setAttribute('aria-live', 'assertive');
+  toast.setAttribute('aria-atomic', 'true');
+  
+  
+  toastHeader.append(headerTitle, closeButton);
+  toast.append(toastHeader, toastBody);
+  toastContainer.append(toast);
+  container.append(toastContainer);
 
-  content.innerHTML = message;
+  toastHeader.innerHTML = `<div><i class="text-light bg-primary p-2 fa-solid fa-signature rounded"></i><span class="ms-2 text-dark">CodeHub</span></div>`
+  toastBody.innerHTML = `${message}`;
+  
+  const bootstrapToast = new bootstrap.Toast(toast);
+  bootstrapToast.show();
 
-  return modal;
+  return bootstrapToast;
 }
