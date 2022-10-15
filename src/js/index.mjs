20456a1ece29afafc573d37de318a2604af77643
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-10-15 13:24:34 +0200

    router

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,25 +1,3 @@
-import * as handler from '../js/handlers/index.mjs';
-// import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import router from "./router.mjs";
 
-
-const path = location.pathname;
-
-console.log(path);
-
-if (path === '/index.html' || path === '/') {
-  handler.setloginFormListener();
-} else if (path === '/register/') {
-  handler.setRegisterFormListener();
-} else if (path === '/feed/') {
-  handler.setGetPosts();
-  handler.setCreatePostFormListener();
-  handler.setUpdatePostFormListener();
-  handler.setSearchPostsFormListener();
-  handler.setFilterPostListener();
-  handler.setLogoutEventListener();
-  handler.setCreateCommentListener();
-  handler.setGetProfile();
-  handler.setGetUsers();
-  handler.setUpdateMediaFormListener();
-  handler.setSearchUsersFormListener();
-}
+router();
---
+++ src/js/router.mjs
@@ -0,0 +1,30 @@
+import * as handler from '../js/handlers/index.mjs';
+
+export default function router() {
+  const path = location.pathname;
+  switch(path) {
+    case '/index.html':
+    handler.setloginFormListener();
+    break;
+    case '/':
+    handler.setloginFormListener();
+    break;
+    case '/register/':
+    handler.setRegisterFormListener();
+    break;
+    case '/feed/':
+    handler.setGetPosts();
+    handler.setCreatePostFormListener();
+    handler.setUpdatePostFormListener();
+    handler.setSearchPostsFormListener();
+    handler.setFilterPostListener();
+    handler.setLogoutEventListener();
+    handler.setCreateCommentListener();
+    handler.setGetProfile();
+    handler.setGetUsers();
+    handler.setUpdateMediaFormListener();
+    handler.setSearchUsersFormListener();
+    break;
+  }
+  
+}
