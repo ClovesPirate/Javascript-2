911d09445eaeecb247bc98b3ab92af075491feed
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-11-02 12:03:07 +0100

    Updated router for hosting

--- src/js/router.mjs
+++ src/js/router.mjs
@@ -5,17 +5,39 @@ import * as handler from '../js/handlers/index.mjs';
  */
 export default function router() {
   const path = location.pathname;
+  const hostPath = '/Javascript-2'
 
   switch(path) {
+    case `${hostPath}/index.html`:
+    handler.setloginFormListener();
+    break;
     case '/index.html':
+    handler.setloginFormListener();
+    break;
+    case `${hostPath}/`:
     handler.setloginFormListener();
     break;
     case '/':
     handler.setloginFormListener();
     break;
+    case `${hostPath}/register/`:
+    handler.setRegisterFormListener();
+    break;
     case '/register/':
     handler.setRegisterFormListener();
     break;
+    case `${hostPath}/feed/`:
+    handler.setCreatePostFormListener();
+    handler.setGetPosts();
+    handler.setUpdatePostFormListener();
+    handler.setSearchPostsFormListener();
+    handler.setFilterPostListener();
+    handler.setLogoutEventListener();
+    handler.setCreateCommentListener();
+    handler.setGetProfile();
+    handler.setGetUsers();
+    handler.setUpdateMediaFormListener();
+    handler.setSearchUsersFormListener();
     case '/feed/':
     handler.setGetPosts();
     handler.setCreatePostFormListener();
@@ -28,6 +50,7 @@ export default function router() {
     handler.setGetUsers();
     handler.setUpdateMediaFormListener();
     handler.setSearchUsersFormListener();
+    console.log('hello');
     break;
   }
 }
